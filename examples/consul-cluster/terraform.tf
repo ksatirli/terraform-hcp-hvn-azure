23d6cd2411d65324f06aab159f5ffea1ef63ba30
Kerim Satirli <k.satirli@gmail.com>
2022-08-12 12:11:39 +0200

    updates version constraints

--- examples/consul-cluster/terraform.tf
+++ examples/consul-cluster/terraform.tf
@@ -4,23 +4,22 @@ terraform {
     # see https://registry.terraform.io/providers/hashicorp/azuread/2.27.0
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.27.0"
+      version = ">= 2.27.0, < 3.0.0"
     }
 
     # see https://registry.terraform.io/providers/hashicorp/azurerm/3.17.0/
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.17.0"
+      version = ">= 3.17.0, < 4.0.0"
     }
 
     # see https://registry.terraform.io/providers/hashicorp/hcp/0.39.0/
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.39.0"
+      version = "0.40.0, < 1.0.0"
     }
   }
 
   # see https://www.terraform.io/docs/language/settings/index.html#specifying-a-required-terraform-version
   required_version = ">= 1.2.0"
 }
-
